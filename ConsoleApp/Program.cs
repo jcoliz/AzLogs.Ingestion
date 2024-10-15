b974419e9a8bdb4eaf032ad799e7420c7c0ebf27
James Coliz <jcoliz@outlook.com>
2024-10-15 09:31:53 -0700

    feat(console): Add initial weather.gov request

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+
 Console.WriteLine("Hello, World!");
+
+using var client = new HttpClient();
+using var request = new HttpRequestMessage()
+{
+    RequestUri = new Uri("https://api.weather.gov/gridpoints/SEW/124,69/forecast")
+};
+request.Headers.UserAgent.Add(ProductInfoHeaderValue.Parse("Weather.Worker/0.0.0"));
+
+var response = await client.SendAsync(request);
+var responseText = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+Console.WriteLine(responseText);
